de06a9fadecb745ae515f7b908f38054073cb984
WalterMarch <38635451+WalterMarch@users.noreply.github.com>
2018-05-28 13:35:35 -0400

    changevarnames
    i was not being used, corrected.
    changed x to j so not to conguse it with coordinate system

--- crazyCatsCradle.py
+++ crazyCatsCradle.py
@@ -3,12 +3,12 @@ import turtle, random
 t = turtle.Turtle()
 
 i = 180
-for x in range(180):
+for j in range(i):
     myPenColor1 = (random.randint(0,100))/100
     myPenColor2 = (random.randint(0,100))/100
     myPenColor3 = (random.randint(0,100))/100
     t.pencolor(myPenColor1,myPenColor2,myPenColor3)
-    t.forward(x*2)
-    t.right(x)
+    t.forward(j*2)
+    t.right(j)
 
 input("Press Enter to continue...")
