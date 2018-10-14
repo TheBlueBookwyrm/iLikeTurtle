5ffeb8926a401d517d15c029816c9d4dd0181fdd
WalterMarch <walter.march@accolade.com>
2018-10-14 08:25:14 -0400

    files modified to be more inline with PEP8 according to PyCharm

--- stars.py
+++ stars.py
@@ -21,25 +21,22 @@ except ValueError:
     exit()
 
 if dotsUpper >= dotsLower:
-    numDots = random.randint(dotsLower,dotsUpper)
+    numDots = random.randint(dotsLower, dotsUpper)
 else:
     print("The value for the upper limit of stars must be larger than the value for the lower limit.") 
     exit()
 
 t = turtle.Turtle()
 s = turtle.Screen()
-s.bgcolor(0,0,0)
+s.bgcolor(0, 0, 0)
 t.pencolor("white")
 t.hideturtle()
 
 limitX, limitY = t.screen.screensize()
 
-#blue, aliceblue, white, lightyellow, yellow, orange, red
-#1,1,1,3,7,12,75
-colorList = []
-colorList.append("blue")
-colorList.append("aliceblue")
-colorList.append("white")
+# blue, aliceblue, white, lightyellow, yellow, orange, red
+# 1,1,1,3,7,12,75
+colorList = ["blue", "aliceblue", "white"]
 for s in range(3):
     colorList.append("lightyellow")
 for s in range(7):
@@ -53,16 +50,16 @@ for i in range(numDots):
     tx, ty = t.position()
     t.forward(1)
     t.penup()
-    angle = random.randint(1,359)
+    angle = random.randint(1, 359)
     t.right(angle)
-    skip = random.randint(1,100)
+    skip = random.randint(1, 100)
     t.forward(skip)
     if tx < (-1 * limitX) or tx > limitX or ty < (-1 * limitY) or ty > limitY:
-        x = random.randint(-1 * limitX,limitX)
-        y = random.randint(-1 * limitY,limitY)
-        t.goto(x,y)
+        x = random.randint(-1 * limitX, limitX)
+        y = random.randint(-1 * limitY, limitY)
+        t.goto(x, y)
     t.pendown()    
-    j = random.randint(0,99) 
+    j = random.randint(0, 99)
     t.pencolor(colorList[j])
 
 input("Press Enter to continue...")
