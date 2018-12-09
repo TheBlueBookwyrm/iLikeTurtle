044bfab15b4fa3ed76715a79660b640f8a9b9ace
WalterMarch <walter.march@accolade.com>
2018-12-09 11:10:41 -0500

    fixed boundary issues

--- flightofthebumblebee.py
+++ flightofthebumblebee.py
@@ -8,6 +8,8 @@ t = turtle.Turtle()
 i = 50
 t.left(90)
 
+limitX, limitY = t.screen.screensize()
+
 print("\nProcess START: " + str(datetime.now()))
 
 for j in range(i):
@@ -19,6 +21,11 @@ for j in range(i):
     t.forward(i)
     turn = random.randint(1, 359)
     t.right(turn)
+
+    tx, ty = t.position()
+    if tx < (-1 * limitX) or tx > limitX or ty < (-1 * limitY) or ty > limitY:
+        t.goto(0, 0)
+
 
 if t.isvisible():
     t.hideturtle()
